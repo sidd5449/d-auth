b66c2893fa29db03e270e76c9fe004be955f2d55
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-06-13 23:34:07 +0530

    minor changes

--- bankServer/index.js
+++ bankServer/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    secret: process.env.SECRET_KEY,
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
     resave: false 
